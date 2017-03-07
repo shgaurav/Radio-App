316952525e989af1c30527ed9dc31045b683152e
Gaurav Sharma <grv7070@gmail.com>
2017-03-07 02:28:20 -0500

    Initial Commit: Finish boilerplate code. Finished coding activity, fragments, station_fragments, adapters and viewholders.

---
+++ app/src/main/java/com/example/gaurav/devslopesradio/holders/StationViewHolder.java
@@ -0,0 +1,15 @@
+package com.example.gaurav.devslopesradio.holders;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * Created by Gaurav on 3/7/2017.
+ */
+
+public class StationViewHolder extends RecyclerView.ViewHolder{
+    public StationViewHolder(View itemView) {
+        super(itemView);
+    }
+}
+
